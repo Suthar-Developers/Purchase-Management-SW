3017e87fafb6606a395af3022d46853c87513226
Kamlesh Suthar <jangirkrt@gmail.com>
2026-06-13 10:00:00 +0530

    Fix text sizes in create PO & PO req module

--- Frontend/src/components/models/PurchaseOrderRequests.jsx
+++ Frontend/src/components/models/PurchaseOrderRequests.jsx
@@ -37,33 +37,29 @@ const PurchaseOrderRequests = () => {
     return (
         <div>
             <div className="mx-5 my-3 pb-4 border-b">
-                <h1 className="text-2xl font-bold">Purchase Order Requests for Approval</h1>
+                <h1 className="text-base font-bold">Purchase Order Requests for Approval</h1>
             </div>
 
             <div className="">
                 <div className="bg-white h-[90%] rounded-xl p-6">
-
                     <div className="flex justify-between items-center mb-5">
-                        <h2 className="text-xl font-bold">Drafted Purchase Orders :-</h2>
-
+                        <h2 className="text-sm font-bold">Drafted Purchase Orders :-</h2>
                     </div>
 
                     <div className="h-[85%] overflow-auto rounded-t-lg">
-
                         <table className="w-full">
-
-                            <thead className="bg-[#4b5ea3] text-white">
+                            <thead className="bg-[#4b5ea3] text-white text-xs">
                                 <tr>
-                                    <th className="w-[8%] p-3 text-center">PO No</th>
-                                    <th className="w-[8%] p-3 text-center">PO Date</th>
-                                    <th className="w-[14%] p-3 text-center">Project</th>
-                                    <th className="w-[12%] p-3 text-center">Vendor</th>
-                                    <th className="w-[12%] p-3 text-center">Order Placed By</th>
-                                    <th className="w-[12%] p-3 text-center">Initiator</th>
-                                    <th className="w-[8%] p-3 text-center">Initiator Number</th>
-                                    <th className="w-[8%] p-3 text-center">Total Amount</th>
-                                    <th className="w-[6%] p-3 text-center">PO Status</th>
-                                    <th className="w-[8%] p-3 text-center">Action</th>
+                                    <th className="w-[8%] p-2 text-center">PO No</th>
+                                    <th className="w-[8%] p-2 text-center">PO Date</th>
+                                    <th className="w-[14%] p-2 text-center">Project</th>
+                                    <th className="w-[12%] p-2 text-center">Vendor</th>
+                                    <th className="w-[12%] p-2 text-center">Order Placed By</th>
+                                    <th className="w-[12%] p-2 text-center">Initiator</th>
+                                    <th className="w-[8%] p-2 text-center">Initiator Number</th>
+                                    <th className="w-[8%] p-2 text-center">Total Amount</th>
+                                    <th className="w-[6%] p-2 text-center">PO Status</th>
+                                    <th className="w-[8%] p-2 text-center">Action</th>
                                 </tr>
                             </thead>
 
@@ -76,9 +72,9 @@ const PurchaseOrderRequests = () => {
                                     </tr>
                                 ) : (
                                     draftedPOs.map((po) => (
-                                        <tr className="border border-b-gray-300 border-x-gray-100" key={po.po_id}>
+                                        <tr className="border border-b-gray-300 border-x-gray-100 text-xs" key={po.po_id}>
 
-                                            <td className="w-[8%] py-3 text-center">{po.po_number}</td>
+                                            <td className="w-[8%] py-3 text-center font-medium">{po.po_number}</td>
                                             <td className="w-[8%] py-3 text-center">{po.order_date ? dayjs(po.order_date).format('DD MMM YYYY') : '-'}</td>
                                             <td className="w-[14%] py-3 text-center">{po.projectName}</td>
                                             <td className="w-[12%] py-3 text-center">{po.vendorName}</td>
@@ -86,7 +82,7 @@ const PurchaseOrderRequests = () => {
                                             <td className="w-[12%] py-3 text-center">{po.initiator}</td>
                                             <td className="w-[8%] py-3 text-center">{po.initiator_number}</td>
                                             <td className="w-[8%] py-3 text-center">{po.grand_total}</td>
-                                            <td className="w-[6%] py-3 text-center">{po.po_status}</td>
+                                            <td className="w-[6%] py-3 text-center font-medium">{po.po_status}</td>
 
                                             <td className="w-[8%] py-3 text-center">
                                                 <Button icon={<i className="fa-notdog fa-solid fa-eye fa-lg hover:cursor-pointer"></i>} onClick={() => openPOViewModel(po)} className="text-blue-700 hover:text-green-600 ml-2 hover:scale-110" />
--- Frontend/src/pages/purchase/PurchaseOrders.jsx
+++ Frontend/src/pages/purchase/PurchaseOrders.jsx
@@ -6,7 +6,6 @@ import PurchaseRequestView from "../../components/models/PurchaseRequestView";
 import Button from "../../components/common/Button";
 
 const PurchaseOrders = () => {
-
     const [approvedPRs, setApprovedPRs] = useState([])
     const [selectedPR, setSelectedPR] = useState(null)
     const [isOpenManualPOForm, setIsOpenManualPOForm] = useState(false)
@@ -38,54 +37,46 @@ const PurchaseOrders = () => {
     return (
         <div>
             <div className="mx-5 my-3 pb-4 border-b">
-                <h1 className="text-2xl font-bold">Create Purchase Order</h1>
+                <h1 className="text-lg font-bold">Create Purchase Order</h1>
             </div>
 
             <div className="">
                 <div className="bg-white w-[80%] h-[90%] rounded-xl p-6">
-
                     <div className="flex justify-between items-center mb-5">
-                        <h2 className="text-xl font-bold">Approved Purchase Requests :-</h2>
-
-                        <Button lable="Create PO Manually" onClick={() => { setSelectedPR(null); setIsOpenManualPOForm(true); }} className="flex items-center justify-center gap-3 py-3 px-6 bg-indigo-600 text-white font-semibold rounded-xl hover:bg-indigo-800 hover:cursor-pointer transition" icon={<i className=" fa-solid fa-pen-to-square text-ms"></i>} />
-
+                        <h2 className="text-base font-bold">Approved Purchase Requests :-</h2>
+                        <Button lable="Create PO Manually" onClick={() => { setSelectedPR(null); setIsOpenManualPOForm(true); }} className="flex items-center justify-center gap-3 py-3 px-6 bg-indigo-600 text-white text-xs font-semibold rounded-xl hover:bg-indigo-800 hover:cursor-pointer transition" icon={<i className=" fa-solid fa-pen-to-square text-ms"></i>} />
                     </div>
 
                     <div className="h-[85%] overflow-auto rounded-t-lg">
                         <table className="w-full">
-
-                            <thead className="bg-[#4b5ea3] text-white">
+                            <thead className="bg-[#4b5ea3] text-white text-xs">
                                 <tr>
-                                    <th className="p-4 text-center">PR No</th>
-                                    <th className="p-4 text-center">PR Date</th>
-                                    <th className="p-4 text-center">Project</th>
-                                    <th className="p-4 text-center">PR Status</th>
-                                    <th className="p-4 text-center">Deliver Before</th>
-                                    <th className="p-4 text-center">Action</th>
+                                    <th className="p-3 text-center">PR No</th>
+                                    <th className="p-3 text-center">PR Date</th>
+                                    <th className="p-3 text-center">Project</th>
+                                    <th className="p-3 text-center">PR Status</th>
+                                    <th className="p-3 text-center">Deliver Before</th>
+                                    <th className="p-3 text-center">Action</th>
                                 </tr>
                             </thead>
 
                             <tbody>
                                 {approvedPRs.length === 0 ? (
                                     <tr>
-                                        <td colSpan="6" className="text-center py-6 text-gray-400">
-                                            No approved requests found
-                                        </td>
+                                        <td colSpan="6" className="text-center py-6 text-gray-400">No approved requests found</td>
                                     </tr>
                                 ) : (
                                     approvedPRs.map((pr) => (
-                                        <tr className="border border-b-gray-300 border-x-gray-100" key={pr.request_id}>
-
+                                        <tr className="border border-b-gray-300 border-x-gray-100 text-xs" key={pr.request_id}>
                                             <td className="py-3 text-center">PR-{pr.request_id}</td>
                                             <td className="py-3 text-center">{pr.created_pr_at ? dayjs(pr.created_pr_at).format('DD MMM YYYY, hh:mm A') : '-'}</td>
                                             <td className="py-3 text-center">{pr.projectName}</td>
                                             <td className="py-3 text-center">{pr.requestStatus}</td>
                                             <td className="py-3 text-center">{pr.deliverBefore ? dayjs(pr.deliverBefore).format('DD MMM YYYY') : '-'}</td>
-
                                             <td className="py-3 text-center">
                                                 <div className="flex justify-center">
-                                                    <Button lable="Create PO" onClick={() => { setSelectedPR(pr); setIsOpenManualPOForm(true); }} className="pr-2 font-semibold border-r hover:text-blue-500 hover:scale-103" />
-                                                    <Button onClick={() => openPRViewModel(pr)} className="text-blue-700 hover:text-green-600 ml-2 hover:scale-110" icon={<i className="fa-notdog fa-solid fa-eye fa-lg hover:cursor-pointer"></i>} />
+                                                    <Button lable="Create PO" onClick={() => { setSelectedPR(pr); setIsOpenManualPOForm(true); }} className="pr-2 font-semibold text-xs border-r hover:text-blue-500 hover:scale-103" />
+                                                    <Button onClick={() => openPRViewModel(pr)} className="text-blue-700 text-xs hover:text-green-600 ml-2 hover:scale-110" icon={<i className="fa-notdog fa-solid fa-eye fa-lg hover:cursor-pointer"></i>} />
                                                 </div>
                                             </td>
                                         </tr>
