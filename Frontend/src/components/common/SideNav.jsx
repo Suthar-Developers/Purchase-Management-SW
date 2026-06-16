b9a282b72ed94420ea92e9a6769d8985387c3745
Kamlesh Suthar <jangirkrt@gmail.com>
2026-06-16 10:00:00 +0530

    Fix spacing in sideNav

--- Frontend/src/components/common/SideNav.jsx
+++ Frontend/src/components/common/SideNav.jsx
@@ -15,9 +15,9 @@ const SideNav = () => {
                 <h1 className='text-center text-xl font-bold'>JRC INTERIORS</h1>
             </div>
 
-            <div className='pb-4'>
+            <div className='pb-2'>
                 <Link to={'/'}>
-                    <div className={`btn py-2.5 rounded w-3xs mb-3 ${isActive('/') ? 'text-white font-semibold bg-gray-800' : ''}`}>
+                    <div className={`btn py-2 rounded w-3xs mb-3 ${isActive('/') ? 'text-white font-semibold bg-gray-800' : ''}`}>
                         <button className='flex items-center gap-2 ml-2 text-left'>
                             <i className="fa-notdog fa-solid fa-house fa-lg"></i>
                             Dashboard
@@ -26,7 +26,7 @@ const SideNav = () => {
                 </Link>
 
                 <Link to={'/projects'}>
-                    <div className={`btn py-2.5 rounded w-3xs mb-3 ${isActive('/projects') ? 'text-white font-semibold bg-gray-800' : ''}`}>
+                    <div className={`btn py-2 rounded w-3xs mb-3 ${isActive('/projects') ? 'text-white font-semibold bg-gray-800' : ''}`}>
                         <button className='flex items-center gap-2 ml-2 text-left'>
                             <i className="fa-regular fa-map fa-lg"></i>
                             Projects
@@ -35,7 +35,7 @@ const SideNav = () => {
                 </Link>
 
                 <Link to={'/vendors'}>
-                    <div className={`btn py-2.5 rounded w-3xs mb-3 ${isActive('/vendors') ? 'text-white font-semibold bg-gray-800' : ''}`}>
+                    <div className={`btn py-2 rounded w-3xs mb-3 ${isActive('/vendors') ? 'text-white font-semibold bg-gray-800' : ''}`}>
                         <button className='flex items-center gap-2 ml-2 text-left'>
                             <i className="fa-solid fa-users-between-lines fa-lg"></i>
                             Vendors
@@ -44,9 +44,9 @@ const SideNav = () => {
                 </Link>
             </div >
 
-            <div className='pb-4'>
+            <div className='pb-2'>
                 <Link to={'/purchase-requests'}>
-                    <div className={`btn py-2.5 rounded w-3xs mb-3 ${isActive('/purchase-requests') ? 'text-white font-semibold bg-gray-800' : ''}`}>
+                    <div className={`btn py-2 rounded w-3xs mb-3 ${isActive('/purchase-requests') ? 'text-white font-semibold bg-gray-800' : ''}`}>
                         <button className='flex items-center gap-2 ml-2 text-left'>
                             <i className="fa-solid fa-list-check fa-lg"></i>
                             Purchase Requests
@@ -55,7 +55,7 @@ const SideNav = () => {
                 </Link>
 
                 <div onClick={() => setOpenPO(!openPO)} className="flex items-center justify-between cursor-pointer">
-                    <div className='btn py-2.5 rounded w-3xs mb-1'>
+                    <div className='btn py-2 rounded w-3xs mb-1'>
                         <button className='flex items-center gap-2 ml-2 text-left'>
                             <i className="fa-solid fa-check-to-slot fa-lg"></i>
                             Purchase Orders
@@ -64,7 +64,7 @@ const SideNav = () => {
                     </div>
                 </div>
 
-                <div className={`flex flex-col gap-2 mb-4 text-sm overflow-hidden transition-all duration-300 ${openPO ? 'max-h-40 opacity-100' : 'max-h-0 opacity-0'}`}>
+                <div className={`flex flex-col gap-1 mb-4 text-xs overflow-hidden transition-all duration-300 ${openPO ? 'max-h-40 opacity-100' : 'max-h-0 opacity-0'}`}>
                     <Link to="/purchase-orders">
                         <div className={`btn py-2 px-7 cursor-pointer hover:text-white ${isActive('/purchase-orders') ? 'text-white font-semibold bg-gray-800' : ''}`}>
                             <span>Create PO</span>
@@ -84,7 +84,7 @@ const SideNav = () => {
                     </Link>
                 </div>
 
-                <div className={`btn py-2.5 rounded w-3xs mb-3 ${isActive('/good-receipt') ? 'text-white font-semibold bg-gray-800' : ''}`}>
+                <div className={`btn py-2 rounded w-3xs mb-1 ${isActive('/good-receipt') ? 'text-white font-semibold bg-gray-800' : ''}`}>
                     <button className='flex items-center gap-2 ml-2 text-left'>
                         <i className="fa-solid fa-receipt fa-lg"></i>
                         Good Receipt
@@ -92,22 +92,22 @@ const SideNav = () => {
                 </div>
             </div>
 
-            <div className='pb-4'>
-                <div className={`btn py-2.5 rounded w-3xs mb-3 ${isActive('/stocks') ? 'text-white font-semibold bg-gray-800' : ''}`}>
+            <div className='pb-2'>
+                <div className={`btn py-2 rounded w-3xs mb-2 ${isActive('/stocks') ? 'text-white font-semibold bg-gray-800' : ''}`}>
                     <button className='flex items-center gap-2 ml-2 text-left'>
                         <i className="fa-solid fa-cart-flatbed fa-lg"></i>
                         Stocks
                     </button>
                 </div>
 
-                <div className={`btn py-2.5 rounded w-3xs mb-3 ${isActive('/bills') ? 'text-white font-semibold bg-gray-800' : ''}`}>
+                <div className={`btn py-2 rounded w-3xs mb-2 ${isActive('/bills') ? 'text-white font-semibold bg-gray-800' : ''}`}>
                     <button className='flex items-center gap-2 ml-2 text-left'>
                         <i className="fa-solid fa-file-invoice fa-lg"></i>
                         Bills
                     </button>
                 </div>
 
-                <div className={`btn py-2.5 rounded w-3xs mb-3 ${isActive('/reports') ? 'text-white font-semibold bg-gray-800' : ''}`}>
+                <div className={`btn py-2 rounded w-3xs mb-2 ${isActive('/reports') ? 'text-white font-semibold bg-gray-800' : ''}`}>
                     <button className='flex items-center gap-2 ml-2 text-left'>
                         <i className="fa-solid fa-square-poll-vertical fa-lg"></i>
                         Reports
@@ -116,7 +116,7 @@ const SideNav = () => {
             </div>
 
             <div>
-                <div className={`btn py-2.5 rounded w-3xs mb-3 ${isActive('/setting') ? 'text-white font-semibold bg-gray-800' : ''}`}>
+                <div className={`btn py-2 rounded w-3xs mb-2 ${isActive('/setting') ? 'text-white font-semibold bg-gray-800' : ''}`}>
                     <button className='flex items-center gap-2 ml-2 text-left'>
                         <i className="fa-solid fa-gear fa-lg"></i>
                         Setting
