12652284d1a65d1d5582bdb40c58f7aac4e59dba
Kamlesh Suthar <jangirkrt@gmail.com>
2026-06-17 10:00:00 +0530

    Fix spacing in Header

--- Frontend/src/components/common/Header.jsx
+++ Frontend/src/components/common/Header.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='flex justify-between w-full h-min py-2 shadow-xl bg-white'>
+    <div className='flex justify-between w-full h-min py-1 shadow-xl bg-white'>
         <div></div>
-      <h1 className='text-xl font-bold'>Purchase Management</h1>
+      <h1 className='text-lg font-bold'>Purchase Management</h1>
       <div className='flex items-center gap-4 mr-5'>
         <div className='flex flex-col items-center'>
             <h3 className='text-sm'>Kamlesh Suthar</h3>
-            <h5 className='text-sm'>Admin</h5>
+            <h5 className='text-xs'>Admin</h5>
         </div>
         <i className="fa-solid fa-circle-user fa-2xl"></i>
       </div>
