0bde5e8ebb0800141d8159aea8deb40cbb09c87d
Kamlesh Suthar <jangirkrt@gmail.com>
2026-06-18 10:00:00 +0530

    Fix spacing in project module

--- Frontend/src/pages/project/Projects.jsx
+++ Frontend/src/pages/project/Projects.jsx
@@ -71,21 +71,21 @@ const Projects = () => {
     return (
         <div className='main-screen w-full h-screen bg-slate-200 overflow-y-auto'>
             <div className='max-w-full h-[80%] bg-white m-5 rounded-2xl overflow-auto'>
-                <h1 className='text-lg font-bold p-6'>All Projects</h1>
-                <div className='flex justify-around items-center w-full px-15 text-center mb-5'>
+                <h1 className='text-base font-bold px-6 py-2'>All Projects</h1>
+                <div className='flex justify-around items-center w-full px-15 text-center mb-3'>
                     <input
-                        className='rounded-lg px-4 py-2 bg-gray-100 text-black text-sm font-bold hover:bg-gray-200 w-full mr-5'
+                        className='rounded-lg px-4 py-2 bg-gray-100 text-black text-xs font-bold hover:bg-gray-200 w-full mr-5'
                         type="search"
                         name="ProjectSearch"
                         placeholder='Search projects...'
                         value={searchProject}
                         onChange={(e) => setSearchProject(e.target.value)}
                     />
-                    <Button lable='+ Add' className='w-25 px-6 py-2 text-white text-sm font-medium bg-blue-600 rounded-lg hover:bg-blue-700 hover:cursor-pointer' onClick={openModel} />
+                    <Button lable='+ Add' className='w-25 px-6 py-2 text-white text-xs font-medium bg-blue-600 rounded-lg hover:bg-blue-700 hover:cursor-pointer' onClick={openModel} />
                 </div>
 
                 <div className='flex flex-col gap-3 w-full overflow-auto rounded-lg'>
-                    <div className='flex justify-around rounded-t-lg text-sm font-medium bg-[#4b5ea3] text-white py-3 mx-2'>
+                    <div className='flex justify-around rounded-t-lg text-xs font-medium bg-[#4b5ea3] text-white py-3 mx-2'>
                         <div className='w-1/8 text-center'>#</div>
                         <div className='w-1/4'>Project Name</div>
                         <div className='w-1/4 text-center'>Project Code</div>
@@ -100,7 +100,7 @@ const Projects = () => {
                     </div>
 
                     {filteredProjects.map((project, index) => (
-                        <div key={project.project_id} className='flex justify-around items-center pb-2 text-xs border-b border-slate-300'>
+                        <div key={project.project_id} className='flex justify-around items-center pb-2 mx-2 text-xs border-b border-slate-300'>
                             <div className='w-1/8 text-center'>{index + 1}</div>
                             <div className='w-1/4'>{project.projectName}</div>
                             <div className='w-1/4 text-center'>{project.projectCode}</div>
@@ -111,9 +111,10 @@ const Projects = () => {
                             <div className='w-1/4 text-center'>{formatDate(project.endDate)}</div>
                             <div className='w-1/4 text-center'>{project.status}</div>
                             <div className='w-1/4 text-center'>{formatDate(project.updated_at)}</div>
-                            <div className='w-1/4 text-center'>
-                                <button onClick={() => openView(project)} className="text-blue-700"><i className="fa-notdog fa-solid fa-eye mr-3 hover:cursor-pointer"></i></button>
-                                <button onClick={() => handleEdit(project)} className="text-green-600"><i className="fa-solid fa-pen-to-square hover:cursor-pointer"></i></button>
+                            <div className='flex w-1/4 justify-center'>
+
+                                <Button onClick={() => openView(project)} className="text-blue-700" icon={<i className="fa-notdog fa-solid fa-eye mr-3 hover:cursor-pointer hover:text-green-600 hover:scale-110"></i>} />
+                                <Button onClick={() => handleEdit(project)} className="text-green-600" icon={<i className="fa-solid fa-pen-to-square hover:cursor-pointer"></i>} />
                             </div>
                         </div>
                     ))}
