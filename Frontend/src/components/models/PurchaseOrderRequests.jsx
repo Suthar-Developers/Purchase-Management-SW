dc84c03cf0792974fa09670ef981c84c9d22cbc9
Kamlesh Suthar <jangirkrt@gmail.com>
2026-06-20 10:00:00 +0530

    Fix spacing in PO request module

--- Frontend/src/components/models/PurchaseOrderRequests.jsx
+++ Frontend/src/components/models/PurchaseOrderRequests.jsx
@@ -41,25 +41,25 @@ const PurchaseOrderRequests = () => {
             </div>
 
             <div className="">
-                <div className="bg-white h-[90%] rounded-xl p-6">
-                    <div className="flex justify-between items-center mb-5">
-                        <h2 className="text-sm font-bold">Drafted Purchase Orders :-</h2>
+                <div className="bg-white h-[90%] rounded-xl px-6">
+                    <div className="flex justify-between items-center mb-3">
+                        <h2 className="text-sm font-semibold">Drafted Purchase Orders :-</h2>
                     </div>
 
                     <div className="h-[85%] overflow-auto rounded-t-lg">
                         <table className="w-full">
                             <thead className="bg-[#4b5ea3] text-white text-xs">
                                 <tr>
-                                    <th className="w-[8%] p-2 text-center">PO No</th>
-                                    <th className="w-[8%] p-2 text-center">PO Date</th>
-                                    <th className="w-[14%] p-2 text-center">Project</th>
-                                    <th className="w-[12%] p-2 text-center">Vendor</th>
-                                    <th className="w-[12%] p-2 text-center">Order Placed By</th>
-                                    <th className="w-[12%] p-2 text-center">Initiator</th>
-                                    <th className="w-[8%] p-2 text-center">Initiator Number</th>
-                                    <th className="w-[8%] p-2 text-center">Total Amount</th>
-                                    <th className="w-[6%] p-2 text-center">PO Status</th>
-                                    <th className="w-[8%] p-2 text-center">Action</th>
+                                    <th className="w-[10%] p-1 text-center">PO No</th>
+                                    <th className="w-[8%] p-1 text-center">PO Date</th>
+                                    <th className="w-[14%] p-1 text-center">Project</th>
+                                    <th className="w-[12%] p-1 text-center">Vendor</th>
+                                    <th className="w-[12%] p-1 text-center">Order Placed By</th>
+                                    <th className="w-[12%] p-1 text-center">Initiator</th>
+                                    <th className="w-[8%] p-1 text-center">Initiator Number</th>
+                                    <th className="w-[8%] p-1 text-center">Total Amount</th>
+                                    <th className="w-[6%] p-1 text-center">PO Status</th>
+                                    <th className="w-[6%] p-1 text-center">Action</th>
                                 </tr>
                             </thead>
 
@@ -74,7 +74,7 @@ const PurchaseOrderRequests = () => {
                                     draftedPOs.map((po) => (
                                         <tr className="border border-b-gray-300 border-x-gray-100 text-xs" key={po.po_id}>
 
-                                            <td className="w-[8%] py-3 text-center font-medium">{po.po_number}</td>
+                                            <td className="w-[10%] py-3 text-center font-medium">{po.po_number}</td>
                                             <td className="w-[8%] py-3 text-center">{po.order_date ? dayjs(po.order_date).format('DD MMM YYYY') : '-'}</td>
                                             <td className="w-[14%] py-3 text-center">{po.projectName}</td>
                                             <td className="w-[12%] py-3 text-center">{po.vendorName}</td>
@@ -84,7 +84,7 @@ const PurchaseOrderRequests = () => {
                                             <td className="w-[8%] py-3 text-center">{po.grand_total}</td>
                                             <td className="w-[6%] py-3 text-center font-medium">{po.po_status}</td>
 
-                                            <td className="w-[8%] py-3 text-center">
+                                            <td className="w-[6%] py-3 text-center">
                                                 <Button icon={<i className="fa-notdog fa-solid fa-eye fa-lg hover:cursor-pointer"></i>} onClick={() => openPOViewModel(po)} className="text-blue-700 hover:text-green-600 ml-2 hover:scale-110" />
                                             </td>
                                         </tr>
