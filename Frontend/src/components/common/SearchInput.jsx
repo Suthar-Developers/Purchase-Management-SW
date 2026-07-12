76f08c9844983abd2b394853d3c5b7eb7a62e7d0
Kamlesh Suthar <161230813+kamleshsuthar19@users.noreply.github.com>
2026-07-12 17:34:08 +0530

    Modify SearchInput to support empty placeholder
    Updated the SearchInput component to allow an empty placeholder and adjusted the input className.

--- Frontend/src/components/common/SearchInput.jsx
+++ Frontend/src/components/common/SearchInput.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const SearchInput = ({
     value,
     onChange,
-    placeholder = "Search...",
+    placeholder = "",
     className = "",
     inputClassName = "",
 }) => {
@@ -14,7 +14,7 @@ const SearchInput = ({
                 value={value}
                 onChange={(e) => onChange(e)}
                 placeholder={placeholder}
-                className={`w-full rounded-lg px-4 py-2 bg-gray-100 text-black text-xs font-bold hover:bg-gray-200 outline-none ${inputClassName}`}
+                className={`${inputClassName}`}
             />
         </div>
     );
